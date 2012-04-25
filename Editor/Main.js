1cbedc6251acd68f3a1df9c1761a67709a7dd4d1
Oleg <olegos83@yandex.ru>
2012-04-25 16:56:36 +0300

    Gradient push to server

--- Editor/Main.js
+++ Editor/Main.js
@@ -11,17 +11,4 @@ function main() {
     
     //init transformer
     stage.trBox = new TrBox();
-    
-    //text test
-    var t = new Text('TextForTest', 'Text and gradient sample', {size: '70px'});
-    stage.addObject(t);
-    t.move(45, 200);
-    
-    var tfill = new Gradient('linear');
-    tfill.addColorStop(0,'#ff0000');
-    tfill.addColorStop(0.5,'#00ff00');
-    tfill.addColorStop(1,'#0000ff');
-    t.color.fill = tfill;
-    
-    ColorChooser(1);
 }
