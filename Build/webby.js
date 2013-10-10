61863a628b971808d0f32fe17034216af68c6e80
olegos83 <olegos83@yandex.ru>
2013-10-11 01:59:10 +0300

    EventListener class refactored and compiled to WebbyJs

--- Build/webby.js
+++ Build/webby.js
@@ -20,6 +20,28 @@ var WebbyJs = {
 	_globals: [],
 	
 	/**
+	 * Unique global numeric id.
+	 * 
+	 * @memberof WebbyJs
+	 * @type {Number}
+	 * 
+	 * @private
+	 */
+	_uniqId: 0,
+	
+	/**
+	 * Global unique id generator.
+	 * 
+	 * @method getUniqId
+	 * @memberof WebbyJs
+	 * 
+	 * @returns {Number} global numeric id.
+	 */
+	getUniqId: function() {
+		return this._uniqId++;
+	},
+	
+	/**
 	 * Browser information.
 	 * 
 	 * @memberof WebbyJs
@@ -436,6 +458,143 @@ WebbyJs.createClass('BaseWebbyJsClass', null,
 		invoke: function(method, args) {
 			method.apply(this, args);
 			return this;
+		}
+	}
+);
+/**
+ * @file WebbyJs events interface and basic event types.
+ * @author Olegos <olegos83@yandex.ru>
+ */
+WebbyJs.import({
+	/**
+	 * Mouse event types.
+	 * 
+	 * @memberof WebbyJs
+	 * @enum {String}
+	 */
+	MouseEvent: {
+	    CLICK: 'click',
+	    DBLCLICK: 'dblclick',
+	    DOWN: 'mousedown',
+	    MOVE: 'mousemove',
+	    UP: 'mouseup',
+	    OVER: 'mouseover',
+	    OUT: 'mouseout',
+	    DRAGSTART: 'dragstart',
+	    DRAG: 'drag',
+	    DRAGEND: 'dragend'
+	},
+	
+	/**
+	 * Keyboard event types.
+	 * 
+	 * @memberof WebbyJs
+	 * @enum {String}
+	 */
+	KeyEvent: {
+	    KEYDOWN: 'keydown',
+	    KEYUP: 'keyup'
+	}
+});
+
+/**
+ * EventListener interface provides events processing support.
+ * 
+ * To add events support, inherit or extend class with EventListener
+ * and declare '_events' object in its constructor.
+ * 
+ * @class EventListener
+ * @memberof WebbyJs
+ */
+WebbyJs.createClass('EventListener', null,
+	/**
+	 * @constructs EventListener
+	 */
+	function() {
+		/**
+		 * Events hash, containing arrays of functions by event type as key.
+		 * 
+		 * @memberof EventListener
+		 * @type {Object}
+		 * 
+		 * @private
+		 */
+		this._events = {};
+	},
+	
+	/**
+	 * Prototype description.
+	 */
+	{
+		/**
+		 * Add event listener for specified event type.
+		 * 
+		 * @method addEventListener
+		 * @memberof EventListener.prototype
+		 * 
+		 * @param {String} type - event type.
+		 * @param {Function} handler - event handler.
+		 * 
+		 * @returns {EventListener} current instance for chaining.
+		 */
+		addEventListener: function(type, handler) {
+			if (!this._events[type]) {
+				this._events[type] = [handler];
+			} else {
+				var e = this._events[type], l = e.length, i;
+				
+				for (i = 0; i < l; i++) {
+					if (e[i] == handler) return this;
+				}
+				
+				e.push(handler);
+			}
+			
+			return this;
+		},
+		
+		/**
+		 * Remove event listener for specified event type.
+		 * 
+		 * @method removeEventListener
+		 * @memberof EventListener.prototype
+		 * 
+		 * @param {String} type - event type.
+		 * @param {Function} handler - event handler, can be ommited to remove all handlers.
+		 * 
+		 * @returns {EventListener} current instance for chaining.
+		 */
+		removeEventListener: function(type, handler) {
+		    if (!handler || !this._events[type]) {
+		    	this._events[type] = [];
+		    } else {
+		    	var e = this._events[type], l = e.length, i;
+		    	
+		    	for (i = 0; i < l; i++) {
+		    		if (e[i] == handler) { e.splice(i, 1); break; }
+		    	}
+		    }
+		    
+		    return this;
+		},
+		
+		/**
+		 * Process handlers for specified event type.
+		 * 
+		 * @method processEvent
+		 * @memberof EventListener.prototype
+		 * 
+		 * @param {Object} evt - event object.
+		 * 
+		 * @returns {EventListener} current instance for chaining.
+		 */
+		processEvent: function(evt) {
+		    if (this._events[evt.type]) {
+		    	var e = this._events[evt.type], l = e.length, i;
+		    	for (i = 0; i < l; i++) e[i].call(this, evt);
+		    }
+		    
+		    return this;
 		}
 	}
 );
--- Source/Core/EventListener.js
+++ Source/Core/EventListener.js
@@ -1,84 +1,15 @@
 /**
- * @file The implementation of WebbyJs class creation.
+ * @file WebbyJs events interface and basic event types.
  * @author Olegos <olegos83@yandex.ru>
  */
-
-/**
- * Base class for all WebbyJs created classes.
- * All created classes are inherited from it.
- * 
- * @class BaseWebbyJsClass
- * @memberof WebbyJs
- */
-WebbyJs.createClass('BaseWebbyJsClass', null,
-	/**
-	 * @constructs BaseWebbyJsClass
-	 */
-	function() {
-		//empty constructor
-	},
-	
-	/**
-	 * Prototype description.
-	 */
-	{
-		/**
-		 * Invoke method with 'this' reference to current instance.
-		 * 
-		 * @method invoke
-		 * @memberof BaseWebbyJsClass.prototype
-		 * 
-		 * @param {Function} method - method to invoke.
-		 * @param {Array} args - method arguments.
-		 * 
-		 * @returns {BaseWebbyJsClass} current instance for chaining.
-		 */
-		invoke: function(method, args) {
-			method.apply(this, args);
-			return this;
-		}
-	}
-);
-
-/*
- * EventListener by OlegoS, 09 Apr 2013
- *
- * Event manager for objects and data driven structures.
- */
-
-
-//ANONYMOUS FUNCTION WRAPPER
-( function() {
-//PRIVATE
-	
-	
-//CONSTRUCTOR
-	/**
-	 * EventListener is used to manage events for objects and data. User can define his own events.
-	 *
-	 * @class EventListener
-	 * @author OlegoS
-	 *
-	 * @constructor
-	 **/
-	var EventListener = function() {
-		/**
-		 * Events container. All events are stored in arrays of functions.
-		 * 
-		 * @property _events
-		 * @type Object
-		 * 
-		 * @private
-		 **/
-		this._events = {};
-	}
-	
-	
-//STATIC
+WebbyJs.import({
 	/**
 	 * Mouse event types.
-	 **/
-	var MouseEvent = {
+	 * 
+	 * @memberof WebbyJs
+	 * @enum {String}
+	 */
+	MouseEvent: {
 	    CLICK: 'click',
 	    DBLCLICK: 'dblclick',
 	    DOWN: 'mousedown',
@@ -89,65 +20,118 @@ WebbyJs.createClass('BaseWebbyJsClass', null,
 	    DRAGSTART: 'dragstart',
 	    DRAG: 'drag',
 	    DRAGEND: 'dragend'
-	};
+	},
 	
 	/**
 	 * Keyboard event types.
-	 **/
-	var KeyEvent = {
+	 * 
+	 * @memberof WebbyJs
+	 * @enum {String}
+	 */
+	KeyEvent: {
 	    KEYDOWN: 'keydown',
 	    KEYUP: 'keyup'
-	};
-	
-//PROTOTYPE
-	//get prototype reference
-	var p = EventListener.prototype;
+	}
+});
+
+/**
+ * EventListener interface provides events processing support.
+ * 
+ * To add events support, inherit or extend class with EventListener
+ * and declare '_events' object in its constructor.
+ * 
+ * @class EventListener
+ * @memberof WebbyJs
+ */
+WebbyJs.createClass('EventListener', null,
+	/**
+	 * @constructs EventListener
+	 */
+	function() {
+		/**
+		 * Events hash, containing arrays of functions by event type as key.
+		 * 
+		 * @memberof EventListener
+		 * @type {Object}
+		 * 
+		 * @private
+		 */
+		this._events = {};
+	},
 	
 	/**
-	 * Add event listener for specified event type.
-	 * 
-	 * @method addEventListener
-	 * @param {String} eventType - type of event.
-	 * @param {Function} listener - event handler.
-	 **/
-	p.addEventListener = function(eventType, listener) {
-		if (!listener) return;
-		var events = this._events[eventType];
+	 * Prototype description.
+	 */
+	{
+		/**
+		 * Add event listener for specified event type.
+		 * 
+		 * @method addEventListener
+		 * @memberof EventListener.prototype
+		 * 
+		 * @param {String} type - event type.
+		 * @param {Function} handler - event handler.
+		 * 
+		 * @returns {EventListener} current instance for chaining.
+		 */
+		addEventListener: function(type, handler) {
+			if (!this._events[type]) {
+				this._events[type] = [handler];
+			} else {
+				var e = this._events[type], l = e.length, i;
+				
+				for (i = 0; i < l; i++) {
+					if (e[i] == handler) return this;
+				}
+				
+				e.push(handler);
+			}
+			
+			return this;
+		},
 		
-		if (!events) this._events[eventType] = [listener]; else {
-			for (var i = 0, l = events.length; i < l; i++) if (events[i] == listener) return;
-			events.push(listener);
+		/**
+		 * Remove event listener for specified event type.
+		 * 
+		 * @method removeEventListener
+		 * @memberof EventListener.prototype
+		 * 
+		 * @param {String} type - event type.
+		 * @param {Function} handler - event handler, can be ommited to remove all handlers.
+		 * 
+		 * @returns {EventListener} current instance for chaining.
+		 */
+		removeEventListener: function(type, handler) {
+		    if (!handler || !this._events[type]) {
+		    	this._events[type] = [];
+		    } else {
+		    	var e = this._events[type], l = e.length, i;
+		    	
+		    	for (i = 0; i < l; i++) {
+		    		if (e[i] == handler) { e.splice(i, 1); break; }
+		    	}
+		    }
+		    
+		    return this;
+		},
+		
+		/**
+		 * Process handlers for specified event type.
+		 * 
+		 * @method processEvent
+		 * @memberof EventListener.prototype
+		 * 
+		 * @param {Object} evt - event object.
+		 * 
+		 * @returns {EventListener} current instance for chaining.
+		 */
+		processEvent: function(evt) {
+		    if (this._events[evt.type]) {
+		    	var e = this._events[evt.type], l = e.length, i;
+		    	for (i = 0; i < l; i++) e[i].call(this, evt);
+		    }
+		    
+		    return this;
 		}
 	}
-
-	/**
-	 * Remove event listener for specified event type.
-	 * 
-	 * @method removeEventListener
-	 * @param {String} eventType - type of event.
-	 * @param {Function} listener - event handler.
-	 **/
-	p.removeEventListener = function(eventType, listener) {
-	    var events = this._events[eventType]; if (!events) return;
-	    
-	    if (!listener) this._events[eventType] = []; else {
-	    	for (var i = 0, l = events.length; i < l; i++) if (events[i] == listener) { events.splice(i, 1); return; }
-	    }
-	}
-
-	/**
-	 * Call listeners for specified event type.
-	 * 
-	 * @method processEvent
-	 * @param {Object} e - event object.
-	 **/
-	p.processEvent = function(e) {
-	    var events = this._events[e.type];
-	    if (events) for (var i = 0, l = events.length; i < l; i++) events[i].call(this, e);
-	}
-	
-	//set up for global use
-	window.MouseEvent = MouseEvent;
-	window.KeyEvent = KeyEvent;
-	window.EventListener = EventListener;
-}() );
+);
