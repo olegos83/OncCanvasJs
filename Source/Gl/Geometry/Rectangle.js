0e91de9c425fcc1055f36195bf645ff42823a22e
olegos83 <olegos83@yandex.ru>
2013-10-15 19:36:23 +0300

    Rectangle code refactored

--- Source/Gl/Geometry/Rectangle.js
+++ Source/Gl/Geometry/Rectangle.js
@@ -1,36 +1,42 @@
 /**
- * @file The implementation of WebbyJs class creation.
+ * @file Rectangle implementation in 2D coordinate system.
  * @author Olegos <olegos83@yandex.ru>
  */
 
 /**
- * EventListener interface provides events processing support.
+ * Rectangle is very important in geometry and rendering calculations.
  * 
- * To add events support, inherit or extend class with EventListener
- * and declare '_events' object in its constructor.
- * 
- * @class EventListener
+ * @class Rectangle
  * @memberof WebbyJs
  */
 WebbyJs.createClass({
 	/**
 	 * Class name.
 	 */
-	name: 'EventListener',
+	name: 'Rectangle',
 	
 	/**
 	 * @constructor
+	 * 
+	 * @param {Point} from - up left point. Default is Point(0, 0).
+	 * @param {Point} to - bottom right point. Default is Point(0, 0).
 	 */
-	construct: function EventListener() {
+	construct: function Rectangle(from, to) {
 		/**
-		 * Events hash, containing arrays of functions by event type as key.
-		 * 
-		 * @memberof EventListener
-		 * @type {Object}
-		 * 
-		 * @private
-		 */
-		this._events = {};
+	     * Up left point of rectangle.
+	     * 
+	     * @memberof Rectangle
+	     * @type {Point}
+	     */
+		this.from = from || new WebbyJs.Point();
+		
+	    /**
+	     * Bottom right point of rectangle.
+	     * 
+	     * @memberof Rectangle
+	     * @type {Point}
+	     */
+		this.to = to || new WebbyJs.Point();
 	},
 	
 	/**
@@ -38,226 +44,198 @@ WebbyJs.createClass({
 	 */
 	proto: {
 		/**
-		 * Add event listener for specified event type.
+		 * Returns top right point of this Rectangle.
 		 * 
-		 * @method addEventListener
-		 * @memberof EventListener.prototype
+		 * @method topRight
+		 * @memberof Rectangle.prototype
 		 * 
-		 * @param {String} type - event type.
-		 * @param {Function} handler - event handler.
-		 * 
-		 * @returns {EventListener} current instance for chaining.
+		 * @returns {Point} top right point of this Rectangle.
 		 */
-		addEventListener: function(type, handler) {
+		topRight: function() {
+		    return new WebbyJs.Point(this.to.x, this.from.y);
+		},
+		
+		/**
+		 * Returns bottom left right point of this Rectangle.
+		 * 
+		 * @method botLeft
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @returns {Point} bottom left point of this Rectangle.
+		 */
+		botLeft: function() {
+		    return new WebbyJs.Point(this.from.x, this.to.y);
+		},
+		
+		/**
+		 * Returns width of this Rectangle.
+		 * 
+		 * @method getWidth
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @returns {Number} width of this Rectangle.
+		 */
+		getWidth: function() {
+		    return this.to.x - this.from.x;
+		},
+		
+		/**
+		 * Returns height of this Rectangle.
+		 * 
+		 * @method getHeight
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @returns {Number} height of this Rectangle.
+		 */
+		getHeight: function() {
+		    return this.to.y - this.from.y;
+		},
+		
+		/**
+		 * Get bounding rectangle of this object.
+		 * 
+		 * @method getBoundRect
+		 * @memberof Geom.prototype
+		 * 
+		 * @returns {Rectangle} a bounding rectangle or null if no rectangle.
+		 */
+		getBoundRect: function() {
+		    return this.clone();
+		},
+		
+		/**
+		 * Returns a center point of this Rectangle.
+		 * 
+		 * @method getCenter
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @returns {Point} a center point of this Rectangle.
+		 */
+		getCenter: function() {
+		    return new WebbyJs.Point((this.from.x + this.to.x) / 2, (this.from.y + this.to.y) / 2);
+		},
+		
+		/**
+		 * Place rectangle around specified Point.
+		 * 
+		 * @method placeAroundPoint
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @param {Point} pt - Center point of the Rectangle.
+		 * @param {Number} dist - distance from center to sides.
+		 * 
+		 * @returns {Rectangle} current instance for chaining.
+		 */
+		placeAroundPoint: function(pt, dist) {
+			var px = pt.x, py = pt.y, from = this.from, to = this.to;
+			
+			from.x = px - dist; from.y = py - dist;
+			to.x = px + dist; to.y = py + dist;
+			
 			return this;
+		},
+		
+		/**
+		 * Set rectangle equal to specified rectangle.
+		 * 
+		 * @method set
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @param {Rectangle} rect - the Rectangle.
+		 * 
+		 * @returns {Rectangle} current instance for chaining.
+		 */
+		set: function(rect) {
+			this.from.set(rect.from);
+			this.to.set(rect.to);
+			
+			return this;
+		},
+		
+		/**
+		 * Set 'from' point up left to 'to' point.
+		 * 
+		 * @method normalize
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @returns {Rectangle} current instance for chaining.
+		 */
+		normalize: function() {
+			var a, from = this.from, to = this.to;
+			
+			if (from.x > to.x) { a = from.x; from.x = to.x; to.x = a; }
+			if (from.y > to.y) { a = from.y; from.y = to.y; to.y = a; }
+			
+			return this;
+		},
+		
+		/**
+		 * Returns true if point is inside this Rectangle.
+		 * 
+		 * @method hasPoint
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @param {Point} pt - point to test.
+		 * 
+		 * @returns {Boolean} true if point is inside this Rectangle.
+		 */
+		hasPoint: function(pt) {
+		    return ( (this.from.x <= pt.x) && (pt.x <= this.to.x) && (this.from.y <= pt.y) && (pt.y <= this.to.y) );
+		},
+		
+		/**
+		 * Check intersection between two rectangles.
+		 * 
+		 * @method intersect
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @param {Rectangle} r - rectangle to test.
+		 * 
+		 * @returns {Boolean} true if rectangles intersect or false otherwise.
+		 */
+		intersect: function(r) {
+		    var w = (Math.abs(this.getWidth()) + Math.abs(r.getWidth())) / 2,
+		    	h = (Math.abs(this.getHeight()) + Math.abs(r.getHeight())) / 2,
+		    	c1 = this.getCenter(), c2 = r.getCenter(),
+		    	dx = c1.x - c2.x, dy = c1.y - c2.y;
+		    
+		    return ( (Math.abs(dx) <= w) && (Math.abs(dy) <= h) );
+		},
+		
+		/**
+		 * Clone this Rectangle.
+		 * 
+		 * @method clone
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @returns {Rectangle} a cloned Rectangle.
+		 */
+		clone: function() {
+		    return new WebbyJs.Rectangle(this.from.clone(), this.to.clone());
+		},
+		
+		/**
+		 * Convert this Rectangle to Path.
+		 * 
+		 * @method toPath
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @returns {Path} result Path.
+		 */
+		toPath: function() {
+		    return new WebbyJs.Path([this.from.clone(), this.topRight(), this.to.clone(), this.botLeft()], true);
+		},
+		
+		/**
+		 * Returns a string representation of this object.
+		 * 
+		 * @method toString
+		 * @memberof Rectangle.prototype
+		 * 
+		 * @returns {String} a string representation of this object.
+		 */
+		toString: function() {
+		    return "[WebbyJs.Rectangle(from:" + this.from.toString() + ", to:" + this.to.toString() + ")]";
 		}
 	}
 });
-
-
-/*
- * Rectangle by OlegoS, 09 Apr 2013
- *
- * Rectangle implementation in 2D coordinate system.
- */
-
-
-//ANONYMOUS FUNCTION WRAPPER
-( function() {
-//PRIVATE
-	
-	
-//CONSTRUCTOR
-	/**
-	 * Rectangle is very important in geometry and rendering calculations.
-	 * Has two points: from - up left point, to - bottom right point.
-	 *
-	 * @class Rectangle
-	 * @author OlegoS
-	 *
-	 * @constructor
-	 * @param {Point} from - up left point. Default is Point(0, 0).
-	 * @param {Point} to - bottom right point. Default is Point(0, 0).
-	 **/
-	var Rectangle = function(from, to) {
-	    /**
-	     * Up left point of rectangle.
-	     * 
-	     * @property from
-	     * @type Point
-	     **/
-		this.from = (from == null ? new Point() : from);
-
-	    /**
-	     * Bottom right point of rectangle.
-	     * 
-	     * @property to
-	     * @type Point
-	     **/
-		this.to = (to == null ? new Point() : to);
-	}
-	
-	
-//STATIC
-	
-	
-//PROTOTYPE
-	//get prototype reference
-	var p = Rectangle.prototype;
-	
-	/**
-	 * Returns top right point of this Rectangle.
-	 * 
-	 * @method topRight
-	 * 
-	 * @return {Point} top right point of this Rectangle.
-	 **/
-	p.topRight = function() {
-	    return new Point(this.to.x, this.from.y);
-	}
-
-	/**
-	 * Returns bottom left right point of this Rectangle.
-	 * 
-	 * @method botLeft
-	 * 
-	 * @return {Point} bottom left point of this Rectangle.
-	 **/
-	p.botLeft = function() {
-	    return new Point(this.from.x, this.to.y);
-	}
-
-	/**
-	 * Returns width of this Rectangle.
-	 * 
-	 * @method getWidth
-	 * 
-	 * @return {Number} width of this Rectangle.
-	 **/
-	p.getWidth = function() {
-	    return this.to.x - this.from.x;
-	}
-
-	/**
-	 * Returns height of this Rectangle.
-	 * 
-	 * @method getHeight
-	 * 
-	 * @return {Number} height of this Rectangle.
-	 **/
-	p.getHeight = function() {
-	    return this.to.y - this.from.y;
-	}
-
-	/**
-	 * Returns a center point of this Rectangle.
-	 * 
-	 * @method getCenter
-	 * 
-	 * @return {Point} a center point of this Rectangle.
-	 **/
-	p.getCenter = function() {
-	    return new Point((this.from.x + this.to.x) / 2, (this.from.y + this.to.y) / 2);
-	}
-
-	/**
-	 * Place rectangle around specified Point.
-	 * 
-	 * @method placeAroundPoint
-	 * @param {Point} pt - Center point of the Rectangle.
-	 * @param {Number} dist - distance from center to sides.
-	 **/
-	p.placeAroundPoint = function(pt, dist) {
-		var px = pt.x, py = pt.y, from = this.from, to = this.to;
-		
-		from.x = px - dist; from.y = py - dist;
-		to.x = px + dist; to.y = py + dist;
-	}
-
-	/**
-	 * Set rectangle equal to specified rectangle.
-	 * 
-	 * @method set
-	 * @param {Rectangle} rect - the Rectangle.
-	 **/
-	p.set = function(rect) {
-		this.from.set(rect.from); this.to.set(rect.to);
-	}
-
-	/**
-	 * Set 'from' point up left to 'to' point.
-	 * 
-	 * @method normalize
-	 **/
-	p.normalize = function() {
-		var a, from = this.from, to = this.to;
-		
-		if (from.x > to.x) { a = from.x; from.x = to.x; to.x = a; }
-		if (from.y > to.y) { a = from.y; from.y = to.y; to.y = a; }
-	}
-
-	/**
-	 * Returns true if point is inside this Rectangle.
-	 * 
-	 * @method hasPoint
-	 * @param {Point} pt - point to test.
-	 * 
-	 * @return {Boolean} true if point is inside this Rectangle.
-	 **/
-	p.hasPoint = function(pt) {
-	    return ( (this.from.x <= pt.x) && (pt.x <= this.to.x) && (this.from.y <= pt.y) && (pt.y <= this.to.y) );
-	}
-
-	/**
-	 * Check intersection between two rectangles.
-	 * 
-	 * @method intersect
-	 * @param {Rectangle} r - rectangle to test.
-	 * 
-	 * @return {Boolean} true if rectangles intersect or false otherwise.
-	 **/
-	p.intersect = function(r) {
-	    var w = (Math.abs(this.getWidth()) + Math.abs(r.getWidth())) / 2,
-	    	h = (Math.abs(this.getHeight()) + Math.abs(r.getHeight())) / 2,
-	    	c1 = this.getCenter(), c2 = r.getCenter(),
-	    	dx = c1.x - c2.x, dy = c1.y - c2.y;
-	    
-	    return ( (Math.abs(dx) <= w) && (Math.abs(dy) <= h) );
-	}
-
-	/**
-	 * Clone this Rectangle.
-	 * 
-	 * @method clone
-	 * 
-	 * @return {Rectangle} a cloned Rectangle.
-	 **/
-	p.clone = function() {
-	    return new Rectangle(this.from.clone(), this.to.clone());
-	}
-
-	/**
-	 * Convert this Rectangle to Path.
-	 * 
-	 * @method toPath
-	 * 
-	 * @return {Path} result Path.
-	 **/
-	p.toPath = function() {
-	    return new Path([this.from.clone(), this.topRight(), this.to.clone(), this.botLeft()], true);
-	}
-
-	/**
-	 * Returns a string representation of this object.
-	 * 
-	 * @method toString
-	 * 
-	 * @return {String} a string representation of this object.
-	 **/
-	p.toString = function() {
-	    return "[Rectangle(from:" + this.from.toString() + ", to:" + this.to.toString() + ")]";
-	}
-	
-	//set up for global use
-	window.Rectangle = Rectangle;
-}() );
