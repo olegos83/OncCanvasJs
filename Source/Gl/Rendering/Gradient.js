4a4adeb3a66792da3d95353e24e20ade93e72c2f
olegos83 <olegos83@yandex.ru>
2013-10-26 02:34:47 +0300

    Gradient refactoring

--- Source/Gl/Rendering/Gradient.js
+++ Source/Gl/Rendering/Gradient.js
@@ -4,7 +4,11 @@
  */
 
 /**
- * Rectangle is very important in geometry and rendering calculations.
+ * Gradient is an array of color stops to make color transition.
+ * Stops are rendered only at indexes between 0 and 100.
+ * 
+ * It can be linear or radial and has two points - 'from' and 'to',
+ * which define gradient rendering coordinates.
  * 
  * @class Gradient
  * @memberof WebbyJs
@@ -18,17 +22,58 @@ WebbyJs.createClass({
 	/**
 	 * @constructor
 	 * 
-	 * @param {Point} from - up left point. Default is Point(0, 0).
-	 * @param {Point} to - bottom right point. Default is Point(0, 0).
+	 * @param {Point} from - start point. Default is Point(0, 0).
+	 * @param {Point} to - end point. Default is Point(0, 0).
+	 * @param {Number} type - gradient type. Can be linear(default) - 0, or radial - 1.
 	 */
-	construct: function Gradient(from, to) {
+	construct: function Gradient(from, to, type) {
 		/**
-	     * Up left point of rectangle.
+	     * Start point.
 	     * 
 	     * @memberof Gradient
 	     * @type {Point}
 	     */
 		this.from = from || new WebbyJs.Point();
+		
+		/**
+	     * End point.
+	     * 
+	     * @memberof Gradient
+	     * @type {Point}
+	     */
+		this.to = to || new WebbyJs.Point();
+		
+		/**
+	     * Gradient type.
+	     * 
+	     * @memberof Gradient
+	     * @type {Number}
+	     */
+		this.type = type || 0;
+		
+	    /**
+	     * Gradient scale koef.
+	     * 
+	     * @property scale
+	     * @type {Number}
+	     */
+	    this.scale = 1;
+	    
+	    /**
+	     * Gradient rotation angle in radians.
+	     * 
+	     * @property rotation
+	     * @type {Number}
+	     */
+	    this.rotation = 0;
+	    
+	    /**
+	     * Array of color stops.
+	     * 
+	     * @memberof Gradient
+	     * @type {Object}
+	     */
+	    this.stops = {};
 	},
 	
 	/**
@@ -48,196 +93,149 @@ WebbyJs.createClass({
 		intersectBounds: function(target) {
 		    return this;
 		}
-	},
-	
-	/**
-	 * Interfaces.
-	 */
-	interfaces: WebbyJs.Geom
+	}
 });
 
 
 
 //ANONYMOUS FUNCTION WRAPPER
 ( function() {
-//PRIVATE
-	
-	
-//CONSTRUCTOR
-	/**
-	 * Gradient may be initialized from colorStops object, which is empty by default.
-	 * Color stop stored as object where key is stop index and value is stop color - colorStop['0.5'] = color.
-	 * 
-	 * @class Gradient
-	 * @author OlegoS
-	 *
-	 * @constructor
-	 * @param {String} type - gradient type. Can be 'linear' or 'radial'.
-	 * @param {Object} colorStops - array of color stops.
-	 */
-	var Gradient = function(type, colorStops) {
-		/**
-	     * Gradient type.
-	     * 
-	     * @property _type
-	     * @type String
-	     * 
-	     * @private
-	     */
-		this._type = (type == null ? 'linear' : type);
-		if (this._type != 'radial') this._type = 'linear';
-		
-	    /**
-	     * Array of color stops.
-	     * 
-	     * @property _colorStops
-	     * @type Array
-	     * 
-	     * @private
-	     */
-	    this._colorStops = (colorStops == null ? {} : colorStops);
-	    
-	    
-	    /**
-	     * Gradient scale koef.
-	     * 
-	     * @property scale
-	     * @type Number
-	     */
-	    this.scale = 1;
-	    
-	    /**
-	     * Gradient rotation angle in radians.
-	     * 
-	     * @property rotation
-	     * @type Number
-	     */
-	    this.rotation = 0;
-	}
-	
-	
-//STATIC
-	
-	
 //PROTOTYPE
 	//get prototype reference
 	var p = Gradient.prototype;
+
+	/**
+	 * Add color stop or change existing stop color at specified index.
+	 * 
+	 * @method addStop
+	 * @memberof Gradient.prototype
+	 * 
+	 * @param {Number} index - index of stop.
+	 * @param {String|Color} color - css color value or Color instance.
+	 * 
+	 * @returns {Gradient} current instance for chaining.
+	 */
+	p.addStop = function(index, color) {
+		this.stops[index] = color;
+		return this;
+	}
 	
 	/**
-	 * GET/SET Gradient type.
+	 * Remove color stop from specified index.
 	 * 
-	 * @method type
-	 * @param {String} type - gradient type. Can be 'linear' or 'radial'.
+	 * @method removeStop
+	 * @memberof Gradient.prototype
 	 * 
-	 * @returns {String} if no args - return current type.
-	 */
-	p.type = function(type) {
-		if (type) {
-			this._type = type;
-			if (this._type != 'radial') this._type = 'linear';
-		} else return this._type;
-	}
-
-	/**
-	 * Add color stop or change existing stop color.
-	 * 
-	 * @method addColorStop
-	 * @param {Number} index - index of stop.
-	 * @param {String} color - css color value.
-	 */
-	p.addColorStop = function(index, color) {
-		this._colorStops[index] = color;
-	}
-
-	/**
-	 * Remove color stop.
-	 * 
-	 * @method removeColorStop
-	 * @param {Number} index - index of stop.
-	 */
-	p.removeColorStop = function(index) {
-		delete this._colorStops[index];
-	}
-
-	/**
-	 * Check if stop exists.
-	 * 
-	 * @method hasStop
 	 * @param {Number} index - index of stop.
 	 * 
-	 * @returns {Boolean} - true if stop exists or false if not.
+	 * @returns {Gradient} current instance for chaining.
 	 */
-	p.hasStop = function(index) {
-		if (this._colorStops[index]) return true; else return false;
+	p.removeStop = function(index) {
+		delete this.stops[index];
+		return this;
 	}
-
+	
 	/**
-	 * Get stop color by its index.
+	 * Get stop color at specified index.
 	 * 
-	 * @method getStopColor
+	 * @method getStop
+	 * @memberof Gradient.prototype
+	 * 
 	 * @param {Number} index - index of stop.
 	 * 
-	 * @returns {String} color of stop with specified index or '' - if there is no stop.
+	 * @returns {String|Color} - stop color or null if stop does not exist.
 	 */
-	p.getStopColor = function(index) {
-		if (this.hasStop(index)) return this._colorStops[index]; else return '';
+	p.getStop = function(index) {
+		return this.stops[index];
 	}
-
+	
 	/**
-	 * Change stop index if such stop exists.
+	 * Change stop index if such stop exists. If new index contains
+	 * stop - it is moved back to the first free index in the direction of change.
 	 * 
-	 * @method setStopIndex
+	 * @method moveStop
+	 * @memberof Gradient.prototype
+	 * 
 	 * @param {Number} oldIndex - index of stop.
 	 * @param {Number} newIndex - new index to set.
+	 * 
+	 * @returns {Gradient} current instance for chaining.
 	 */
-	p.setStopIndex = function(oldIndex, newIndex) {
-		var c = this.getStopColor(oldIndex);
+	p.moveStop = function(oldIndex, newIndex) {
+		var stops = this.stops, s = stops[oldIndex];
 		
-		if (c != '') {
-			this.removeColorStop(oldIndex);
-			this.addColorStop(newIndex, c);
+		if (s) {
+			delete stops[oldIndex];
+			
+			if (stops[newIndex]) {
+				var d = (newIndex < oldIndex ? -1 : 1);
+				while (stops[newIndex]) newIndex += d;
+			}
+			
+			stops[newIndex] = s;
 		}
-	}
-
-	/**
-	 * Get color stop indexes by color.
-	 * 
-	 * @method getStopIndexes
-	 * @param {String} color - color to test. If no color passed - return all indexes.
-	 * 
-	 * @returns {Array} array of stop indexes with specified color or null if no stops.
-	 */
-	p.getStopIndexes = function(color) {
-		var indexes = [];
-		for (var i in this._colorStops) if ( !color || (this._colorStops[i] == color) ) indexes.push(i);
 		
-		if (indexes.length == 0) indexes = null;
+		return this;
+	}
+	
+	/**
+	 * Get stop indexes by color.
+	 * 
+	 * @method getIndexes
+	 * @memberof Gradient.prototype
+	 * 
+	 * @param {String|Color} color - color to test. If no color passed - return all indexes, containing stops.
+	 * 
+	 * @returns {Array} array of stop indexes - can be empty if no stops found.
+	 */
+	p.getIndexes = function(color) {
+		var indexes = [], stops = this.stops, i;
+		
+		if (color == null) for (i in stops) indexes.push(i);
+		else for (i in stops) if (stops[i] == color) indexes.push(i);
+		
 		return indexes;
 	}
-
+	
 	/**
 	 * Clear all color stops.
 	 * 
 	 * @method clearStops
+	 * @memberof Gradient.prototype
+	 * 
+	 * @returns {Gradient} current instance for chaining.
 	 */
-	p.clearStops = function() {
-		this._colorStops = {};
+	p.clear = function() {
+		this.stops = {};
+		return this;
 	}
-
+	
 	/**
-	 * Apply Gradient to canvas gradient instance - simply add all color stops.
+	 * Set all stops from source gradient to current instance.
+	 * 
+	 * Source stops will owerwrite all existing data, but will not
+	 * touch unaffected indexes.
 	 * 
 	 * @method setStops
-	 * @param {CanvasGradient} gr - gradient.
+	 * @memberof Gradient.prototype
+	 * 
+	 * @param {Gradient} gr - source gradient.
+	 * 
+	 * @returns {Gradient} current instance for chaining.
 	 */
 	p.setStops = function(gr) {
-		for (var i in this._colorStops) gr.addColorStop(i, this._colorStops[i]);
+		var stops = this.stops; gr = gr.stops;
+		
+		for (var i in gr) stops[i] = gr[i];
+		return this;
 	}
-
+	
 	/**
 	 * Convert to CanvasGradient from start to end point.
 	 * 
 	 * @method toCanvasGradient
+	 * @memberof Gradient.prototype
+	 * 
 	 * @param {CanvasRenderingContext2d} ctx - canvas context.
 	 * @param {Point} from - start point.
 	 * @param {Point} to - end point.
@@ -245,12 +243,15 @@ WebbyJs.createClass({
 	 * @returns {CanvasGradient} CanvasGradient instance.
 	 */
 	p.toCanvasGradient = function(ctx, from, to) {
+		//check coords
+		if (from) this.from.set(from);
+		if (to) this.to.set(to);
+		
 		//init vars
-		var c = new Point((from.x + to.x) / 2, (from.y + to.y) / 2);
+		var c = new WebbyJs.Point((from.x + to.x) / 2, (from.y + to.y) / 2), gr = null;
 		
 		//create gradient instance
-		var gr = null;
-		switch (this.type()) {
+		switch (this.type) {
 			case 'linear':
 				from.scale(this.scale, this.scale, c);
 				to.scale(this.scale, this.scale, c);
@@ -268,36 +269,37 @@ WebbyJs.createClass({
 		}
 		
 		//return canvas gradient
-		this.setStops(gr);
+		this.setStops(gr); //for (var i in stops) gr.addColorStop(i, stops[i]);
 		return gr;
 	}
-
+	
 	/**
 	 * Clone this gradient.
 	 * 
 	 * @method clone
+	 * @memberof Gradient.prototype
 	 * 
 	 * @returns {Gradient} a cloned gradient.
 	 */
 	p.clone = function() {
-	    var gr = new Gradient(this._type);
-	    this.setStops(gr);
+	    var gr = new WebbyJs.Gradient(this.from.clone(), this.to.clone(), this.type).setStops(this);
 	    
 	    gr.scale = this.scale;
 	    gr.rotation = this.rotation;
 	    
 	    return gr;
 	}
-
+	
 	/**
 	 * Returns a string representation of this object.
 	 * 
 	 * @method toString
+	 * @memberof Gradient.prototype
 	 * 
 	 * @returns {String} a string representation of this object.
 	 */
 	p.toString = function() {
-	    return "[Gradient(type: " + this.type() + ")]";
+	    return "[Gradient(type: " + this.stops + ")]";
 	}
 	
 	//set up for global use
