e3268375440c1abdb17ce81bc3e4a26631d8b4d8
olegos83 <olegos83@yandex.ru>
2013-10-29 00:12:29 +0200

    Coding base graphics object

--- Source/Gl/Rendering/Graphics.js
+++ Source/Gl/Rendering/Graphics.js
@@ -4,7 +4,10 @@
  */
 
 /**
- * Rectangle is very important in geometry and rendering calculations.
+ * Graphics is a base to all renderable objects. Can contain chldren,
+ * which are rendered relativly to parent. Must have global uniq id.
+ * 
+ * Implements EventListener to support events.
  * 
  * @class Graphics
  * @memberof WebbyJs
@@ -18,17 +21,97 @@ WebbyJs.createClass({
 	/**
 	 * @constructor
 	 * 
-	 * @param {Point} from - up left point. Default is Point(0, 0).
-	 * @param {Point} to - bottom right point. Default is Point(0, 0).
+	 * @param {String} id - Graphics id. If ommited - uniq id is generated.
 	 */
-	construct: function Graphics(from, to) {
+	construct: function Graphics(id) {
 		/**
-	     * Up left point of rectangle.
+		 * Events hash, containing arrays of functions by event type as key.
+		 * 
+		 * @memberof Graphics
+		 * @type {Object}
+		 * 
+		 * @private
+		 */
+		this._events = {};
+		
+		/**
+		 * Graphics object uniq id.
+		 * 
+		 * @memberof Graphics
+		 * @type {String}
+		 * 
+		 * @private
+		 */
+	    this._id = '';
+	    
+	    /**
+		 * Graphics children by id cache.
+		 * 
+		 * @memberof Graphics
+		 * @type {Object}
+		 * 
+		 * @private
+		 */
+	    this._childrenById = {};
+	    
+	    /**
+		 * Data array for child graphics objects.
+		 * 
+		 * @memberof Graphics
+		 * @type {Array}
+		 */
+		this.dp_storage = [];
+	    
+	    /**
+	     * Reference to parent layer.
 	     * 
 	     * @memberof Graphics
-	     * @type {Point}
+	     * @type {Layer}
 	     */
-		this.from = from || new WebbyJs.Point();
+	    this.layer = null;
+	    
+	    /**
+	     * Graphics visibility. Default is true.
+	     * 
+	     * @memberof Graphics
+	     * @type {Boolean}
+	     */
+	    this.visible = true;
+	    
+	    /**
+	     * Transformation matrix.
+	     * 
+	     * @memberof Graphics
+	     * @type Matrix
+	     */
+	    this.matrix = new WebbyJs.Matrix();
+	    
+	    /**
+	     * Bounding rectangle.
+	     * 
+	     * @memberof Graphics
+	     * @type Path
+	     */
+	    this.bounds = null;
+	    
+	    /**
+	     * Object rendering style.
+	     * 
+	     * @memberof Graphics
+	     * @type {Style}
+	     */
+	    this.style = new WebbyJs.Style();
+	    
+	    //init
+	    this.id(id || 'gr_' + WebbyJs.getUniqId());
+	    
+	    this.addEventListener(WebbyJs.DataEvent.ADD, this.dataHandlers.onAdd);
+	    
+	    this.addEventListener(WebbyJs.DataEvent.REMOVE, this.dataHandlers.onRemove);
+	    this.addEventListener(WebbyJs.DataEvent.REPLACE, this.dataHandlers.onReplace);
+	    
+	    this.addEventListener(WebbyJs.DataEvent.CHANGE, this.dataHandlers.onChange);
+	    this.addEventListener(WebbyJs.DataEvent.CLEAR, this.dataHandlers.onClear);
 	},
 	
 	/**
@@ -36,295 +119,251 @@ WebbyJs.createClass({
 	 */
 	proto: {
 		/**
-		 * Check intersection between this and target bounding rectangle.
+	     * Graphics data event handlers.
+	     * 
+	     * @memberof Graphics.prototype
+	     * @type {Boolean}
+	     */
+	    dataHandlers: {
+	    	onAdd: function(e) {},
+	    	onRemove: function(e) {},
+	    	onReplace: function(e) {},
+	    	onChange: function(e) {},
+	    	onClear: function(e) {}
+	    },
+		
+		/**
+		 * GET/SET id of graphics object. Id must be uniq, otherwise it doesn't change.
 		 * 
-		 * @method intersectBounds
+		 * @method id
 		 * @memberof Graphics.prototype
 		 * 
-		 * @param {Rectangle} target - target to test.
+		 * @param {String} id - id to set.
+		 * 
+		 * @returns {Graphics|String} current instance for chaining or current id, if called without arguments.
+		 */
+		id: function(id) {
+			if (!id) return this._id;
+			
+			if (!WebbyJs._idCache[id]) {
+				delete WebbyJs._idCache[this._id];
+				
+				this._id = id;
+				WebbyJs._idCache[id] = true;
+			}
+			
+		    return this;
+		},
+		
+		/**
+		 * Set object rendering style.
+		 * 
+		 * @method setStyle
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {Object} style - initial style.
 		 * 
 		 * @returns {Graphics} current instance for chaining.
 		 */
-		intersectBounds: function(target) {
+		setStyle: function(style) {
+			this.style.set(style);
+			if (this.layer) this.layer.redraw();
+			
+			return this;
+		},
+		
+		/**
+		 * Get graphics child by its id.
+		 * 
+		 * @method getChildById
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {String} id - child id.
+		 * 
+		 * @returns {Graphics} child or null if child not found.
+		 */
+		getChildById: function(id) {
+			return this._childrenById[id];
+		},
+		
+		/**
+		 * Set child graphics index.
+		 * 
+		 * @method setChildIndex
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {Graphics} child - child reference.
+		 * @param {Number} index - new index position.
+		 * 
+		 * @returns {Graphics} current instance for chaining.
+		 */
+		setChildIndex: WebbyJs.DataProvider.prototype.setItemIndex,
+		
+		/**
+		 * Get child graphics index.
+		 * 
+		 * @method getChildIndex
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {Graphics} child - child reference.
+		 * 
+		 * @returns {Number} - child index or -1 if child not found.
+		 */
+		getChildIndex: WebbyJs.DataProvider.prototype.indexOf,
+		
+		/**
+		 * Get child graphics at specified index.
+		 * 
+		 * @method getChildAt
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {Number} index - index to check.
+		 * 
+		 * @returns {Graphics} - child or null if child not found.
+		 */
+		getChildAt: WebbyJs.DataProvider.prototype.itemAt,
+		
+		/**
+		 * Remove all children from this graphics object.
+		 * 
+		 * @method removeAllChildren
+		 * @memberof Graphics.prototype
+		 * 
+		 * @returns {Graphics} current instance for chaining.
+		 */
+		removeAllChildren: WebbyJs.DataProvider.prototype.clear,
+		
+		/**
+		 * Get number of children in this graphics object.
+		 * 
+		 * @method numChildren
+		 * @memberof Graphics.prototype
+		 * 
+		 * @returns {Number} - number of children.
+		 */
+		numChildren: WebbyJs.DataProvider.prototype.length,
+		
+		/**
+		 * Get bounding rectangle of the object.
+		 * 
+		 * @method getBoundRect
+		 * @memberof Graphics.prototype
+		 * 
+		 * @returns {Rectangle} - bounding rectangle.
+		 */
+		getBoundRect: function() {
+		    return null;
+		},
+		
+		/**
+		 * Move object by x/y deltas.
+		 * 
+		 * @method move
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {Number} dx - X delta.
+		 * @param {Number} dy - Y delta.
+		 * 
+		 * @returns {Graphics} current instance for chaining.
+		 */
+		move: function(dx, dy) {
+		    this.matrix.translate(dx, dy);
+		    if (this.layer) this.layer.redraw();
+		    
 		    return this;
+		},
+		
+		/**
+		 * Scale object from pivot.
+		 * 
+		 * @method scale
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {Number} scX - x scale koef.
+		 * @param {Number} scY - y scale koef.
+		 * @param {Point} pivot - pivot to scale from.
+		 * 
+		 * @returns {Graphics} current instance for chaining.
+		 */
+		scale: function(scX, scY, pivot) {
+			this.matrix.scale(scX, scY, pivot);
+			if (this.layer) this.layer.redraw();
+		    
+		    return this;
+		},
+		
+		/**
+		 * Rotate object around pivot.
+		 * 
+		 * @method rotate
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {Number} angle - rotation angle (in radians).
+		 * @param {Point} pivot - pivot to rotate around.
+		 * 
+		 * @returns {Graphics} current instance for chaining.
+		 */
+		rotate: function(angle, pivot) {
+		    this.matrix.rotate(angle, pivot);
+		    if (this.layer) this.layer.redraw();
+		    
+		    return this;
+		},
+		
+		/**
+		 * Apply matrix transformation.
+		 * 
+		 * @method matrixTransform
+		 * @memberof Graphics.prototype
+		 * 
+		 * @param {Matrix} m - matrix.
+		 * 
+		 * @returns {Graphics} current instance for chaining.
+		 */
+		matrixTransform: function(m) {
+		    this.matrix.multiply(m);
+		    if (this.layer) this.layer.redraw();
+		    
+		    return this;
+		},
+		
+		/**
+		 * Dispose object - free objects resources, such as id and event listeners.
+		 * This method can be called to force memory clear and prevent leaks.
+		 * 
+		 * @method dispose
+		 * @memberof Graphics.prototype
+		 */
+		dispose: function() {
+			delete WebbyJs._idCache[this._id];
+			
+			this._events = null;
+			this._childrenById = null;
+			
+			this.dp_storage = null;
+			this.layer = null;
+			
+			this.matrix = null;
+			this.bounds = null;
+			
+			this.style = null;
+		},
+		
+		/**
+		 * Returns a string representation of this object.
+		 * 
+		 * @method toString
+		 * @memberof Graphics.prototype
+		 * 
+		 * @returns {String} a string representation of this object.
+		 */
+		toString: function() {
+		    return "[Graphics(id:" + this._id + ")]";
 		}
 	},
 	
 	/**
 	 * Interfaces.
 	 */
-	interfaces: WebbyJs.Geom
+	interfaces: [WebbyJs.Geom, WebbyJs.DataProvider]
 });
-
-
-
-//ANONYMOUS FUNCTION WRAPPER
-( function() {
-//PRIVATE
-	
-	
-//CONSTRUCTOR
-	/**
-	 * Graphics is a base to all graphic objects, but it can not be used itself.
-	 * Inherited from EventListener to support events.
-	 * 
-	 * @class Graphics
-	 * @super EventListener
-	 * @author OlegoS
-	 *
-	 * @constructor
-	 * @param {String} id - Graphics id. Default is "".
-	 */
-	var Graphics = function(id) {
-		//initialize base class
-		EventListener.call(this);
-
-	   /**
-	    * Graphics id.
-	    * 
-	    * @property id
-	    * @type String
-	    */
-	    this.id = (id == null ? '' : id);
-
-	   /**
-	    * Reference to parent layer.
-	    * 
-	    * @property layer
-	    * @type Layer
-	    */
-	    this.layer = null;
-
-	   /**
-	    * Graphics visibility. Default is true.
-	    * 
-	    * @property visible
-	    * @type Boolean
-	    */
-	    this.visible = true;
-	    
-	    /**
-	     * Transformation matrix.
-	     * 
-	     * @property matrix
-	     * @type Matrix
-	     */
-	    this.matrix = new Matrix();
-	    
-	    /**
-	     * Object bounds shape. Mostly used for internal calculations in some cases.
-	     * 
-	     * @property bounds
-	     * @type Path
-	     */
-	    this.bounds = null;
-	    
-	    /**
-	     * Object rendering style - color, stroke width, opacity, etc.
-	     * More object specific styles can be added dynamically.
-	     * 
-	     * @property style
-	     * @type Number
-	     */
-	    this.style = { strokeWidth: 1, opacity: 1, strokeColor: '#000000', fillColor: '#ffffff', shadowColor: '', shadowBlur: 0 };
-	}
-
-	//extend from EventListener
-	inheritProto(Graphics, EventListener);
-	
-	
-//STATIC
-	
-	
-//PROTOTYPE
-	//get prototype reference
-	var p = Graphics.prototype;
-	
-	/**
-	 * Make object draggable.
-	 * 
-	 * @method startDrag
-	 */
-	p.startDrag = function() {
-	    this.addEventListener(MouseEvent.DOWN, Stage.dragStart);
-	}
-
-	/**
-	 * Stop object draggable ability.
-	 * 
-	 * @method stopDrag
-	 */
-	p.stopDrag = function() {
-	    this.removeEventListener(MouseEvent.DOWN, Stage.dragStart);
-	}
-
-	/**
-	 * Set object style.
-	 * 
-	 * @method setStyle
-	 * @param {Object} style - style to set.
-	 */
-	p.setStyle = function(style) {
-		for (var p in style) this.style[p] = style[p];
-	}
-
-	/**
-	 * Clone object style.
-	 * 
-	 * @method cloneStyle
-	 * 
-	 * @returns {Object} cloned object style.
-	 */
-	p.cloneStyle = function() {
-		var style = this.style, res = {};
-		for (var p in style) res[p] = style[p];
-		return res;
-	}
-
-	/**
-	 * Get bounding rectangle of the object.
-	 * 
-	 * @method getBoundRect
-	 * 
-	 * @returns {Rectangle} - bounding rectangle.
-	 */
-	p.getBoundRect = function() {
-	    var s = this.bounds.clone();
-	    s.matrixTransform(this.matrix);
-	    return s.getBoundRect();
-	}
-
-	/**
-	 * Get object center point.
-	 * 
-	 * @method getCenter
-	 * 
-	 * @returns {Point} - center point.
-	 */
-	p.getCenter = function() {
-	    return this.getBoundRect().getCenter();
-	}
-
-	/**
-	 * Get object width.
-	 * 
-	 * @method getWidth
-	 * 
-	 * @returns {Number} - width.
-	 */
-	p.getWidth = function() {
-	    return this.getBoundRect().getWidth();
-	}
-
-	/**
-	 * Get object height.
-	 * 
-	 * @method getHeight
-	 * 
-	 * @returns {Number} - height.
-	 */
-	p.getHeight = function() {
-	    return this.getBoundRect().getHeight();
-	}
-
-	/**
-	 * Move object by x/y deltas.
-	 * 
-	 * @method move
-	 * @param {Number} dx - X delta.
-	 * @param {Number} dy - Y delta.
-	 */
-	p.move = function(dx, dy) {
-	    this.matrix.translate(dx, dy);
-	    //this.dirty = true;
-	}
-
-	/**
-	 * Scale object from pivot.
-	 * 
-	 * @method scale
-	 * @param {Number} scX - x scale koef.
-	 * @param {Number} scY - y scale koef.
-	 * @param {Point} pivot - pivot to scale from.
-	 */
-	p.scale = function(scX, scY, pivot) {
-		this.matrix.scale(scX, scY, pivot);
-	}
-
-	/**
-	 * Rotate object around pivot.
-	 * 
-	 * @method rotate
-	 * @param {Number} angle - rotation angle (in radians).
-	 * @param {Point} pivot - pivot to rotate around.
-	 */
-	p.rotate = function(angle, pivot) {
-	    this.matrix.rotate(angle, pivot);
-	}
-
-	/**
-	 * Apply matrix transformation.
-	 * 
-	 * @method matrixTransform
-	 * @param {Matrix} m - matrix.
-	 */
-	p.matrixTransform = function(m) {
-	    this.matrix.multiply(m);
-	}
-
-	/**
-	 * Mirror object acording to orientation.
-	 * 
-	 * @method mirror
-	 * @param {String} orientation - 'horiz' - horizontal, 'vert' - vertical.
-	 */
-	p.mirror = function(orientation) {
-		Path.prototype.mirror.call(this, orientation);
-	}
-
-	/**
-	 * Align object in rectangle by base.
-	 * 
-	 * @method align
-	 * @param {String} base - align base: 'left', 'right', 'center', 'top', 'bottom', 'vert'.
-	 * @param {Rectangle} rect - align rectangle.
-	 */
-	p.align = function(base, rect) {
-		Path.prototype.align.call(this, base, rect);
-	}
-
-	/**
-	 * Place object into specified rectangle.
-	 * 
-	 * @method placeIntoRect
-	 * @param {Rectangle} tR - the Rectangle.
-	 */
-	p.placeIntoRect = function(tR) {
-		Path.prototype.placeIntoRect.call(this, tR);
-	}  
-
-	/**
-	 * Place object around specified Point.
-	 * 
-	 * @method placeAroundPoint
-	 * @param {Point} pt - center point.
-	 * @param {Number} dist - distance from center to sides.
-	 */
-	p.placeAroundPoint = function(pt, dist) {
-		Path.prototype.placeAroundPoint.call(this, pt, dist);
-	}
-
-	/**
-	 * Returns a string representation of this object.
-	 * 
-	 * @method toString
-	 * 
-	 * @returns {String} a string representation of this object.
-	 */
-	p.toString = function() {
-	    return "[Graphics(id:" + this.id + ")]";
-	}
-	
-	//set up for global use
-	window.Graphics = Graphics;
-}() );
