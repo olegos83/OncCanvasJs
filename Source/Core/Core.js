36c514f7eab0ef1f0e16ff94fb5f50baf7a1065b
OlegoS <olegos83@yandex.ru>
2014-02-04 00:41:39 +0200

    Core is ready for basic testing

--- Source/Core/Core.js
+++ Source/Core/Core.js
@@ -208,7 +208,7 @@ var w = {
 		if (this.typeOf(base) != 'Function') this.err('Invalid base class');
 
 		child.prototype = new base();
-		w.empty(child.prototype);
+		this.empty(child.prototype);
 
 		child.prototype.constructor = child;
 	},
@@ -220,16 +220,16 @@ var w = {
 	 * @memberof w
 	 *
 	 * @param {Function} target - target class reference.
-	 * @param {Object|Function} ... - single or more interfaces.
+	 * @param {Object} ... - one or more interfaces.
 	 */
 	implement: function(target) {
 		if (this.typeOf(target) != 'Function') this.err('Invalid implementation target');
 
 		for (var i = 1, l = arguments.length; i < l; i++) {
-			var o = arguments[i], t = w.typeOf(o);
+			var o = arguments[i], t = this.typeOf(o);
 
-			if (t == 'Function') w.copy(o.prototype, target.prototype);
-			else if (t == 'Object') w.copy(o, target.prototype);
+			if (t == 'Function') this.copy(o.prototype, target.prototype);
+			else if (t == 'Object') this.copy(o, target.prototype);
 		}
 	}
 
@@ -245,11 +245,9 @@ var w = {
 	 * 	  construct: function ClassName(args) { ... },
 	 *    base: baseClassReference,
 	 *    statics: { static members },
-	 *    implements: interfaceReference,
+	 *    implements: interface || [interfaces],
 	 * 	  proto: { prototype members },
 	 * };
-	 *
-	 * @returns {W} created class.
 	 */
 	create: function(opt) {
 		if (this.typeOf(opt) != 'Object') this.err('Invalid class options');
@@ -258,262 +256,269 @@ var w = {
 		var name = opt.construct.name;
 		if (name == '' || this.typeOf(name) != 'String') this.err('Invalid class name');
 
-		var def = {}; def[name] = opt.construct;
+		var construct = opt.construct, def = {};
+		def[name] = costruct;
 
 		this.define(def);
-		this.W.statics.call(created, this.W);
+		this._classes[name] = construct;
 
-		return opt.construct.statics(opt.statics).extend(opt.base).implement(opt.implements, opt.proto);
+		construct.create = this.W.create;
+		if (this.typeOf(opt.statics) == 'Object') this.copy(opt.statics, construct);
+
+		var base = opt.base || this.W;
+
+		if (base instanceof this.W) {
+			this.inherit(construct, base);
+			this.implement(construct, opt.implements, opt.proto);
+		}
+		else this.err('Base class must be WebbyJs compatible');
 	}
 };
-
-/**
- * Base class for all WebbyJs created classes.
- *
- * @class W
- * @memberof w
- */
-w.define({
-	/**
-	 * @constructor
-	 */
-	W: function W() {}
-});
-
-
-
-/**
- * Add W static members.
- */
-w.W.statics({
-	/**
-	 * Create instance of this class.
-	 *
-	 * @method create
-	 * @memberof W
-	 *
-	 * @returns {W} created instance.
-	 */
-	create: function() {
-		return new this();
-	}
-
-
-
-/**
- * Implement W prototype.
- */
-}).implement({
-	/**
-	 * Set properties in current instance.
-	 *
-	 * @method set
-	 * @memberof W.prototype
-	 *
-	 * @param {W} src - source object.
-	 *
-	 * @returns {W} current instance for chaining.
-	 */
-	set: function(src) {
-		for (var p in src) if (this.hasOwnProperty(p)) this[p] = src[p];
-		return this;
-	},
-
-	/**
-	 * Check if this object is equal to source object by class and properties.
-	 *
-	 * @method isEqual
-	 * @memberof W.prototype
-	 *
-	 * @param {W} src - source object.
-	 *
-	 * @returns {Boolean} true if this is equal to source or false otherwise.
-	 */
-	isEqual: function(src) {
-		if (this.constructor != src.constructor) return false;
-
-		for (var p in this) {
-			if (this.hasOwnProperty(p) && src.hasOwnProperty(p)) {
-				if (this[p] != src[p]) return false;
-			} else return false;
-		}
-
-		return true;
-	},
-
-	/**
-	 * Reset all properties. It resets:
-	 *
-	 * 'Number' to 0,
-	 * 'String' to '',
-	 *
-	 * 'Object' to {},
-	 * 'Array' to [],
-	 *
-	 * 'W' calls W.reset() and 'Function', 'null' and 'undefined' are skipped.
-	 *
-	 * @method reset
-	 * @memberof W.prototype
-	 *
-	 * @returns {W} current instance for chaining.
-	 */
-	reset: function() {
-		for (var p in this) if (this.hasOwnProperty(p)) {
-			var v = this[p];
-
-			if (v) {
-				var t = w.typeOf(v);
-				if (t == 'Function') continue;
-
-				if (v.reset) v.reset();
-
-				else if (t == 'Number') this[p] = 0;
-				else if (t == 'String') this[p] = '';
-
-				else if (t == 'Object') this[p] = {};
-				else if (t == 'Array') this[p] = [];
-			}
-		}
-
-		return this;
-	},
-
-	/**
-	 * Free objects resources from memory.
-	 *
-	 * @method free
-	 * @memberof W.prototype
-	 *
-	 * @returns {W} current instance for chaining.
-	 */
-	free: function() {
-		for (var p in this) if (this.hasOwnProperty(p)) this[p] = null;
-		return this;
-	},
-
-	/**
-	 * Default toString method.
-	 *
-	 * @method toString
-	 * @memberof W.prototype
-	 *
-	 * @returns {String} current instance as string.
-	 */
-	toString: function() {
-		return '[' + this.constructor.name + ']';
-	},
-
-	/**
-	 * Get current instance as Object.
-	 *
-	 * @method toObject
-	 * @memberof W.prototype
-	 *
-	 * @returns {Object} current instance as object.
-	 */
-	toObject: function() {
-		var obj = {};
-		for (var p in this) if (this.hasOwnProperty(p)) obj[p] = this[p];
-		return obj;
-	},
-
-	/**
-	 * Get/Set current instance in XML format.
-	 *
-	 * @method xml
-	 * @memberof W.prototype
-	 *
-	 * @param {String} data - XML source string.
-	 *
-	 * @returns {W|String} XML string or current instance for chaining.
-	 */
-	xml: function(data) {
-		if (data) {
-			return this;
-		}
-
-		return '<xml><empty /></xml>';
-	},
-
-	/**
-	 * Get/Set current instance in JSON format.
-	 *
-	 * @method json
-	 * @memberof W.prototype
-	 *
-	 * @param {String} data - JSON source string.
-	 *
-	 * @returns {W|String} JSON string or current instance for chaining.
-	 */
-	json: function(data) {
-		if (data) {
-			return this;
-		}
-
-		return '{ "json": "empty" }';
-	},
-
-	/**
-	 * Dump current instance to log.
-	 *
-	 * @method dump
-	 * @memberof W.prototype
-	 *
-	 * @returns {W} current instance for chaining.
-	 */
-	dump: function() {
-		w.log(this.toString());
-
-		for (var p in this) {
-			w.log(p + ":" + w.typeOf(this[p]) + " = " + this[p]);
-		}
-
-		return this;
-	},
-
-	/**
-	 * Clone current instance.
-	 *
-	 * @method clone
-	 * @memberof W.prototype
-	 *
-	 * @returns {W} cloned instance.
-	 */
-	clone: function() {
-		var cloned = new this.constructor();
-
-		for (var p in this) if (this.hasOwnProperty(p)) {
-			var o = this[p];
-
-			if (o.clone) cloned[p] = o.clone(); else {
-				cloned[p] = (typeof o == 'object' ? this.clone.call(o) : o);
-			}
-		}
-
-		return cloned;
-	},
-
-	/**
-	 * Invoke method with execution context set to current instance.
-	 *
-	 * @method invoke
-	 * @memberof W.prototype
-	 *
-	 * @param {Function} method - method to invoke.
-	 * @param {Array} args - method arguments.
-	 *
-	 * @returns {W} current instance for chaining.
-	 */
-	invoke: function(method, args) {
-		method.apply(this, args);
-		return this;
-	}
-});
 
 /**
  * Init WebbyJs core.
  */
 w.invoke(function() {
-	//secure core members from overriding
+	/**
+	 * Define basic WebbyJs class - a base for all WebbyJs created classes.
+	 *
+	 * @class W
+	 * @memberof w
+	 */
+	this.define({
+		/**
+		 * @constructor
+		 * @param {Object} opt - creation options.
+		 */
+		W: function W(opt) {
+			this.set(opt);
+		}
+	});
+
+	/**
+	 * Set factory method to create instances.
+	 *
+	 * @method create
+	 * @memberof W
+	 *
+	 * @param {Object} opt - creation options.
+	 *
+	 * @returns {W} created instance.
+	 */
+	this.W.create = function(opt) {
+		return new this(opt);
+	};
+
+	/**
+	 * Implement W.prototype.
+	 */
+	this.implement(this.W, {
+		/**
+		 * Set one or more properties in current instance.
+		 *
+		 * @method set
+		 * @memberof W.prototype
+		 *
+		 * @param {String|Object} opt - option name or multiply options hash.
+		 * @param {*} val - value for single option.
+		 *
+		 * @returns {W} current instance for chaining.
+		 */
+		set: function(opt, val) {
+			if (opt != null) {
+				if (val != null) this[opt] = val; else for (var p in opt) this[p] = opt[p];
+			}
+
+			return this;
+		},
+
+		/**
+		 * Compare itself to target object.
+		 * If deep flag is set - all non-primitive properties are recursivly checked to be equal.
+		 *
+		 * @method equalTo
+		 * @memberof W.prototype
+		 *
+		 * @param {Object} target - target  reference.
+		 * @param {Boolean} deep - deep flag.
+		 *
+		 * @returns {Boolean} true if all its properties are equal to corresponding ones in target.
+		 */
+		equalTo: function(target, deep) {
+			return false;
+		},
+
+		/**
+		 * Reset all properties. It resets:
+		 *
+		 * 'Number' to 0,
+		 * 'String' to '',
+		 *
+		 * 'Object' to {},
+		 * 'Array' to [],
+		 *
+		 * 'W' calls W.reset() and 'Function', 'null' and 'undefined' are skipped.
+		 *
+		 * @method reset
+		 * @memberof W.prototype
+		 *
+		 * @returns {W} current instance for chaining.
+		 */
+		reset: function() {
+			for (var p in this) if (this.hasOwnProperty(p)) {
+				var v = this[p];
+
+				if (v) {
+					var t = w.typeOf(v);
+					if (t == 'Function') continue;
+
+					if (v.reset) v.reset();
+
+					else if (t == 'Number') this[p] = 0;
+					else if (t == 'String') this[p] = '';
+
+					else if (t == 'Object') this[p] = {};
+					else if (t == 'Array') this[p] = [];
+				}
+			}
+
+			return this;
+		},
+
+		/**
+		 * Free objects resources from memory.
+		 *
+		 * @method free
+		 * @memberof W.prototype
+		 *
+		 * @returns {W} current instance for chaining.
+		 */
+		free: function() {
+			for (var p in this) if (this.hasOwnProperty(p)) this[p] = null;
+			return this;
+		},
+
+		/**
+		 * Default toString method.
+		 *
+		 * @method toString
+		 * @memberof W.prototype
+		 *
+		 * @returns {String} current instance as string.
+		 */
+		toString: function() {
+			return '[' + this.constructor.name + ']';
+		},
+
+		/**
+		 * Get current instance as Object.
+		 *
+		 * @method toObject
+		 * @memberof W.prototype
+		 *
+		 * @returns {Object} current instance as object.
+		 */
+		toObject: function() {
+			var obj = {};
+			for (var p in this) if (this.hasOwnProperty(p)) obj[p] = this[p];
+			return obj;
+		},
+
+		/**
+		 * Get/Set current instance in XML format.
+		 *
+		 * @method xml
+		 * @memberof W.prototype
+		 *
+		 * @param {String} data - XML source string.
+		 *
+		 * @returns {W|String} XML string or current instance for chaining.
+		 */
+		xml: function(data) {
+			if (data) {
+				return this;
+			}
+
+			return '<xml><empty /></xml>';
+		},
+
+		/**
+		 * Get/Set current instance in JSON format.
+		 *
+		 * @method json
+		 * @memberof W.prototype
+		 *
+		 * @param {String} data - JSON source string.
+		 *
+		 * @returns {W|String} JSON string or current instance for chaining.
+		 */
+		json: function(data) {
+			if (data) {
+				return this;
+			}
+
+			return '{ "json": "empty" }';
+		},
+
+		/**
+		 * Dump current instance to log.
+		 *
+		 * @method dump
+		 * @memberof W.prototype
+		 *
+		 * @returns {W} current instance for chaining.
+		 */
+		dump: function() {
+			w.log(this.toString());
+
+			for (var p in this) {
+				w.log(p + ":" + w.typeOf(this[p]) + " = " + this[p]);
+			}
+
+			return this;
+		},
+
+		/**
+		 * Clone current instance.
+		 *
+		 * @method clone
+		 * @memberof W.prototype
+		 *
+		 * @returns {W} cloned instance.
+		 */
+		clone: function() {
+			var cloned = new this.constructor();
+
+			for (var p in this) if (this.hasOwnProperty(p)) {
+				var o = this[p];
+
+				if (o.clone) cloned[p] = o.clone(); else {
+					cloned[p] = (typeof o == 'object' ? this.clone.call(o) : o);
+				}
+			}
+
+			return cloned;
+		},
+
+		/**
+		 * Invoke method with execution context set to current instance.
+		 *
+		 * @method invoke
+		 * @memberof W.prototype
+		 *
+		 * @param {Function} method - method to invoke.
+		 * @param {Array} args - method arguments.
+		 *
+		 * @returns {W} current instance for chaining.
+		 */
+		invoke: function(method, args) {
+			method.apply(this, args);
+			return this;
+		}
+	});
+
+	/**
+	 * Secure core members from being overrided.
+	 */
 	this.copy(this, this._core);
 });
