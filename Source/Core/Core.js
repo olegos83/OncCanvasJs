016e35a55dcfffc819125d682e9f34bc4f9015e7
OlegoS <olegos83@yandex.ru>
2014-02-06 01:52:47 +0200

    Core bugfixes and improvments

--- Source/Core/Core.js
+++ Source/Core/Core.js
@@ -132,10 +132,10 @@ var w = {
 	 * @memberof w
 	 *
 	 * @param {Function} method - method to invoke.
-	 * @param {Array} args - method arguments.
+	 * @param {*} ... - method arguments.
 	 */
-	invoke: function(method, args) {
-		method.apply(this, args);
+	invoke: function(method) {
+        Array.prototype.shift.call(arguments).apply(this, arguments);
 	},
 
 	/**
@@ -149,24 +149,33 @@ var w = {
 	 * @returns {String} objects class name or '' for undefined or null.
 	 */
 	typeOf: function(obj) {
-		return (obj == null ? '' : obj.constructor.name);
+		return (obj == null ? '' : obj.classname || obj.constructor.name);
 	},
 
 	/**
-	 * Define source members inside WebbyJs.
+	 * Define members inside WebbyJs.
 	 *
 	 * @method define
 	 * @memberof w
 	 *
-	 * @param {Object} src - members source.
+	 * @param {String|Object} m - member name or multiply members hash.
+	 * @param {*} v - reference to single member.
+	 * @param {Boolean} c - class definition flag.
 	 */
-	define: function(src) {
-		if (this.typeOf(src) != 'Object') this.err('Failed to define members');
+	define: function(m, v, c) {
+		var t = this.typeOf(m);
 
-		for (var m in src) {
+		if (t == 'Object') {
+			for (var p in m) {
+				if (this._core[p]) this.err('Failed to override core member: ' + p);
+				this[p] = m[p];
+			}
+		} else if (t == 'String') {
 			if (this._core[m]) this.err('Failed to override core member: ' + m);
-			this[m] = src[m];
+			if (c) this._classes[m] = v;
+			this[m] = v;
 		}
+		else this.err('Failed to define members');
 	},
 
 	/**
@@ -211,6 +220,7 @@ var w = {
 		this.empty(child.prototype);
 
 		child.prototype.constructor = child;
+		child.prototype.superclass = base.prototype;
 	},
 
 	/**
@@ -220,18 +230,26 @@ var w = {
 	 * @memberof w
 	 *
 	 * @param {Function} target - target class reference.
-	 * @param {Object} ... - one or more interfaces.
+	 * @param {Object|Function|Array} ... - one or more interfaces.
 	 */
 	implement: function(target) {
 		if (this.typeOf(target) != 'Function') this.err('Invalid implementation target');
 
-		for (var i = 1, l = arguments.length; i < l; i++) {
-			var o = arguments[i], t = this.typeOf(o);
+		var proto = target.prototype, classname = proto.classname,
+			construct = proto.constructor, superclass = proto.superclass;
 
-			if (t == 'Function') this.copy(o.prototype, target.prototype);
-			else if (t == 'Object') this.copy(o, target.prototype);
+		for (var i = 1, l = arguments.length; i < l; i++) {
+			var t = this.typeOf(arguments[i]);
+
+			if (t == 'Function') this.copy(arguments[i].prototype, proto);
+			else if (t == 'Object') this.copy(arguments[i], proto);
+			else if (t == 'Array') this.implement.call(this, target, arguments[i]);
 		}
-	}
+
+		proto.classname = classname;
+		proto.constructor = construct;
+		proto.superclass = superclass;
+	},
 
 	/**
 	 * Create new class inside WebbyJs.
@@ -251,27 +269,23 @@ var w = {
 	 */
 	create: function(opt) {
 		if (this.typeOf(opt) != 'Object') this.err('Invalid class options');
-		if (this.typeOf(opt.construct) != 'Function') this.err('Invalid constructor');
 
-		var name = opt.construct.name;
-		if (name == '' || this.typeOf(name) != 'String') this.err('Invalid class name');
-
-		var construct = opt.construct, def = {};
-		def[name] = costruct;
-
-		this.define(def);
-		this._classes[name] = construct;
-
-		construct.create = this.W.create;
-		if (this.typeOf(opt.statics) == 'Object') this.copy(opt.statics, construct);
-
-		var base = opt.base || this.W;
+		var construct = opt.construct, base = opt.base || this.W, name = construct.name;
 
 		if (base instanceof this.W) {
+			if (this.typeOf(construct) != 'Function') this.err('Invalid constructor');
+			if (name == '' || this.typeOf(name) != 'String') this.err('Invalid class name');
+
+			this.define(name, construct, true);
+			construct.prototype.classname = name;
+
+			construct.create = this.W.create;
+			if (this.typeOf(opt.statics) == 'Object') this.copy(opt.statics, construct);
+
 			this.inherit(construct, base);
 			this.implement(construct, opt.implements, opt.proto);
 		}
-		else this.err('Base class must be WebbyJs compatible');
+		else this.err('Base class is not WebbyJs compatible');
 	}
 };
 
@@ -280,20 +294,18 @@ var w = {
  */
 w.invoke(function() {
 	/**
-	 * Define basic WebbyJs class - a base for all WebbyJs created classes.
+	 * Base class for all WebbyJs created classes.
 	 *
-	 * @class W
+	 * @constructor
 	 * @memberof w
+	 *
+	 * @param {Object} opt - creation options.
 	 */
-	this.define({
-		/**
-		 * @constructor
-		 * @param {Object} opt - creation options.
-		 */
-		W: function W(opt) {
-			this.set(opt);
-		}
-	});
+	function W(opt) {
+		this.set(opt);
+	}
+
+	this.define('W', W, true);
 
 	/**
 	 * Set factory method to create instances.
@@ -301,12 +313,17 @@ w.invoke(function() {
 	 * @method create
 	 * @memberof W
 	 *
-	 * @param {Object} opt - creation options.
+	 * @param {*} ... - constructor arguments.
 	 *
 	 * @returns {W} created instance.
 	 */
-	this.W.create = function(opt) {
-		return new this(opt);
+	this.W.create = function() {
+		if (arguments.length == 0) return new this();
+
+		var o = new this();
+		this.apply(o, arguments);
+
+		return o;
 	};
 
 	/**
@@ -314,50 +331,67 @@ w.invoke(function() {
 	 */
 	this.implement(this.W, {
 		/**
+		 * Class name.
+		 *
+		 * @memberof W.prototype
+		 * @type {String}
+		 */
+		classname: 'W',
+
+		/**
+		 * Superclass prototype reference.
+		 *
+		 * @memberof W.prototype
+		 * @type {Object}
+		 */
+		superclass: this.W,
+
+		/**
 		 * Set one or more properties in current instance.
 		 *
 		 * @method set
 		 * @memberof W.prototype
 		 *
-		 * @param {String|Object} opt - option name or multiply options hash.
-		 * @param {*} val - value for single option.
+		 * @param {String|Object} m - property name or multiply properties hash.
+		 * @param {*} v - value for single property.
 		 *
 		 * @returns {W} current instance for chaining.
 		 */
-		set: function(opt, val) {
-			if (opt != null) {
-				if (val != null) this[opt] = val; else for (var p in opt) this[p] = opt[p];
+		set: function(m, v) {
+			if (m) {
+				var t = typeof m;
+
+				if (t == 'object') for (var p in m) this[p] = m[p];
+				else if (t == 'string') this[m] = v;
 			}
 
 			return this;
 		},
 
 		/**
-		 * Compare itself to target object.
-		 * If deep flag is set - all non-primitive properties are recursivly checked to be equal.
+		 * Unset one or more object properties.
 		 *
-		 * @method equalTo
+		 * @method unset
 		 * @memberof W.prototype
 		 *
-		 * @param {Object} target - target  reference.
-		 * @param {Boolean} deep - deep flag.
+		 * @param {String|Array|Object} ... - property name, array of names or multiply properties hash.
 		 *
-		 * @returns {Boolean} true if all its properties are equal to corresponding ones in target.
+		 * @returns {W} current instance for chaining.
 		 */
-		equalTo: function(target, deep) {
-			return false;
+		unset: function() {
+			for (var i = 0, l = arguments.length; i < l; i++) {
+				var arg = arguments[i], t = w.typeOf(arg);
+
+				if (t == 'String') delete this[arg];
+				else if (t == 'Object') for (var p in arg) if (arg.hasOwnProperty(p)) delete this[p];
+				else if (t == 'Array') for (var j = 0, jl = arg.length; j < jl; j++) delete this[arg[j]];
+			}
+
+			return this;
 		},
 
 		/**
-		 * Reset all properties. It resets:
-		 *
-		 * 'Number' to 0,
-		 * 'String' to '',
-		 *
-		 * 'Object' to {},
-		 * 'Array' to [],
-		 *
-		 * 'W' calls W.reset() and 'Function', 'null' and 'undefined' are skipped.
+		 * Reset all native properties to their default values.
 		 *
 		 * @method reset
 		 * @memberof W.prototype
@@ -365,37 +399,65 @@ w.invoke(function() {
 		 * @returns {W} current instance for chaining.
 		 */
 		reset: function() {
-			for (var p in this) if (this.hasOwnProperty(p)) {
-				var v = this[p];
-
-				if (v) {
-					var t = w.typeOf(v);
-					if (t == 'Function') continue;
-
-					if (v.reset) v.reset();
-
-					else if (t == 'Number') this[p] = 0;
-					else if (t == 'String') this[p] = '';
-
-					else if (t == 'Object') this[p] = {};
-					else if (t == 'Array') this[p] = [];
-				}
-			}
-
+			this.constructor.call(this);
 			return this;
 		},
 
 		/**
-		 * Free objects resources from memory.
+		 * Empty object - delete all its properties.
 		 *
-		 * @method free
+		 * @method empty
 		 * @memberof W.prototype
 		 *
 		 * @returns {W} current instance for chaining.
 		 */
-		free: function() {
-			for (var p in this) if (this.hasOwnProperty(p)) this[p] = null;
+		empty: function() {
+			for (var p in this) if (this.hasOwnProperty(p)) delete this[p];
 			return this;
+		},
+
+		/**
+		 * Clean current instance - delete all non-native properties.
+		 *
+		 * @method clean
+		 * @memberof W.prototype
+		 *
+		 * @returns {W} current instance for chaining.
+		 */
+		clean: function() {
+			var c = new this.constructor();
+			for (var p in this) if (c.hasOwnProperty(p) == false) delete this[p];
+			return this;
+		},
+
+		/**
+		 * Compare current instance to target object.
+		 * If deep flag is set - all non-primitive properties are recursivly checked to be equal.
+		 *
+		 * @method equals
+		 * @memberof W.prototype
+		 *
+		 * @param {Object} target - target  reference.
+		 * @param {Boolean} deep - deep flag.
+		 *
+		 * @returns {Boolean} true if all its properties are equal to corresponding ones in target.
+		 */
+		equals: function(target, deep) {
+			return false;
+		},
+
+		/**
+		 * Get all property names from current instance.
+		 *
+		 * @method props
+		 * @memberof W.prototype
+		 *
+		 * @returns {Array} array of property names.
+		 */
+		props: function() {
+			var a = [];
+			for (var p in this) if (this.hasOwnProperty(p)) a.push(p);
+			return a;
 		},
 
 		/**
@@ -407,7 +469,7 @@ w.invoke(function() {
 		 * @returns {String} current instance as string.
 		 */
 		toString: function() {
-			return '[' + this.constructor.name + ']';
+			return '[' + this.classname + ']';
 		},
 
 		/**
@@ -507,12 +569,12 @@ w.invoke(function() {
 		 * @memberof W.prototype
 		 *
 		 * @param {Function} method - method to invoke.
-		 * @param {Array} args - method arguments.
+		 * @param {*} ... - method arguments.
 		 *
 		 * @returns {W} current instance for chaining.
 		 */
-		invoke: function(method, args) {
-			method.apply(this, args);
+		invoke: function(method) {
+			Array.prototype.shift.call(arguments).apply(this, arguments);
 			return this;
 		}
 	});
